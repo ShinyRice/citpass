dc64e1531a0cc2b8d3993e1d23613bf531b17ce4
ShinyRice <14126351+ShinyRice@users.noreply.github.com>
2020-08-16 09:54:34 +0200

    Started working on initialization of the database file. Need to access environment variables somehow.

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-  // First, it's necessary to know how many arguments have been passed. This first case below
-  // executes when just the binary's name has been invoked,
+  /* First, it's necessary to know how many arguments have been passed. This first case below
+  * executes when just the binary's name has been invoked, */
   if(argc == 1) {
     printf("citpass requires an argument. Possible arguments are:\n");
     printf("init - Create the file where passwords will be stored, located at $HOME/.local/share/citpass/passwords\n");
@@ -14,8 +15,8 @@ int main(int argc, char *argv[])
     printf("get - Retrieve a password from the file\n");
   }
   else if(argc == 2){
-    // This is the case where the binary's name plus another argument has been passed.
-    // Now, it's necessary to parse the command line argument passed to the program, so
+    /* This is the case where the binary's name plus another argument has been passed.
+    * Now, it's necessary to parse the command line argument passed to the program, so */
     int arginit = strncmp(argv[1], "init", 5);
     int argadd = strncmp(argv[1], "add", 5);
     int arglist = strncmp(argv[1], "list", 5);
@@ -23,17 +24,24 @@ int main(int argc, char *argv[])
     int argget = strncmp(argv[1], "get", 5);
 
     if (arginit == 0) {
-    // Initialization
-
-      printf("Placeholder for init.\n");
-
-    /* Empty file creation */
+      /* Initialization
+      * Before doing anything, we need to know whether or not the application folder in ~/.local/share
+      * and the file within exists. If both exist, then nothing is done. If the folder exists, but the file doesn't,
+      */
+      if(access("/home/shinyrice/.local/share/citpass", F_OK) != -1) {
+        printf("The folder at /home/shinyrice/.local/share/citpass exists.\n");
+      }
+      else {
+        printf("The folder at /home/shinyrice/.local/share/citpass doesn't exist.\n");
+        printf("Creating it.");
+        /* Empty file creation */
+      }
 
     /* File encryption */
 
     }
     else if(argadd == 0) {
-    // Addition of password
+    /* Addition of password */
 
       printf("Placeholder for add.\n");
 
@@ -47,7 +55,7 @@ int main(int argc, char *argv[])
 
     }
     else if(arglist == 0) {
-    // Addition of password
+    /* Addition of password */
 
       printf("Placeholder for list.\n");
 
@@ -61,7 +69,7 @@ int main(int argc, char *argv[])
 
     }
     else if(argrm == 0) {
-    // Removal of password
+    /* Removal of password */
 
       printf("Placeholder for rm.\n");
 
@@ -77,7 +85,7 @@ int main(int argc, char *argv[])
 
     }
     else if(argget == 0) {
-    // Retrieval of password
+    /* Retrieval of password */
 
       printf("Placeholder for get.\n");
 
@@ -103,8 +111,8 @@ int main(int argc, char *argv[])
     }
   }
   else if(argc > 2) {
-    // citpass will require a second argument when retrieving and removing a password, the title of such a password.
-    //
+    /* citpass will require a second argument when retrieving and removing a password, the title of such a password.
+    * As such, it'll be here when the time comes. */
       printf("Too many arguments supplied.\n");
   }
   return 0;
